ab6e11d30d3644d46927e3523876a8e3e9ba88dd
insanniity <cleydson.cg@live.com>
2021-03-11 17:59:59 -0300

    Heroes data

--- src/main/java/com/insannity/heroes/config/HeroesTable.java
+++ src/main/java/com/insannity/heroes/config/HeroesTable.java
@@ -1,15 +1,9 @@
 package com.insannity.heroes.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import org.apache.commons.lang3.StringUtils;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
