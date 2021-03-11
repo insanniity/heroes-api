5618412882cb5307e4045e0948136cf8cd3a5c66
insanniity <cleydson.cg@live.com>
2021-03-11 18:43:01 -0300

    Service and Repository

---
+++ src/main/java/com/insannity/heroes/repository/HeroesRepository.java
@@ -0,0 +1,9 @@
+package com.insannity.heroes.repository;
+
+import com.insannity.heroes.document.Heroes;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface HeroesRepository extends CrudRepository<Heroes, String>{
+}
